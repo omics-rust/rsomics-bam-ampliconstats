5639b86a3be8207d7d171e63d55ba4be20b7e2c6
Bengerthelorf <bengerlorf@hotmail.com>
2026-05-29 05:19:29 +0800

    test(bam-ampliconstats): add criterion bench harness

---
+++ benches/bench.rs
@@ -0,0 +1,23 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use std::hint::black_box;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn bench_bam_ampliconstats(c: &mut Criterion) {
+    let bin = env!("CARGO_BIN_EXE_rsomics-bam-ampliconstats");
+    let manifest = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let primers = manifest.join("tests/golden/primers.bed");
+    let bam = manifest.join("tests/golden/amplicons.bam");
+    c.bench_function("rsomics-bam-ampliconstats golden", |b| {
+        b.iter(|| {
+            let out = Command::new(black_box(bin))
+                .args([primers.to_str().unwrap(), bam.to_str().unwrap()])
+                .output()
+                .unwrap();
+            assert!(out.status.success());
+        });
+    });
+}
+
+criterion_group!(benches, bench_bam_ampliconstats);
+criterion_main!(benches);
